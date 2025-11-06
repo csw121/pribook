c1a7ed4827314f3b5bf5b2ab5d2c82c5d17031d3
csw <solstry911@gmail.com>
2025-11-06 12:16:07 +0900

    11.06 12/15 이게 맞나?

--- bookmarket/src/main/java/com/bookmarket/BookmarketApplication.java
+++ bookmarket/src/main/java/com/bookmarket/BookmarketApplication.java
@@ -2,12 +2,16 @@ package com.bookmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// [CTO 수정] 'JPA 스위치'의 '부품 수입(import)'을 '정확하게' 합니다.
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+// [CTO 수정] 'JPA 스위치'를 '올바르게' 켭니다.
+@EnableJpaRepositories(basePackages = "com.bookmarket")
 @SpringBootApplication
 public class BookmarketApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookmarketApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookmarketApplication.class, args);
+    }
 
 }
